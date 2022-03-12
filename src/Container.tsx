dfc8ddb45ab242d95703b22c592a1d75723c6b5a
sntkn <mail.tsurumi@gmail.com>
2022-03-12 17:27:09 +0900

    Divide the cards by column

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,14 +1,19 @@
-import { FC, useState, useCallback } from 'react'
+import { FC, useState, useCallback, useEffect } from 'react'
 import { Card } from './Card'
 import update from 'immutability-helper'
 
 const style = {
   width: 400,
+  height: '100%',
+  border: '1px dashed gray',
+  padding: '0.5rem 1rem',
+  margin: '.5rem',
 }
 
 export interface Item {
   id: number
   text: string
+  column: number
 }
 
 export interface ContainerState {
@@ -20,30 +25,37 @@ export const Container: FC = () => {
     {
       id: 1,
       text: 'Write a cool JS library',
+      column: 0,
     },
     {
       id: 2,
       text: 'Make it generic enough',
+      column: 0,
     },
     {
       id: 3,
       text: 'Write README',
+      column: 0,
     },
     {
       id: 4,
       text: 'Create some examples',
+      column: 0,
     },
     {
       id: 5,
       text: 'Spam in Twitter and IRC to promote it (note that this element is taller than the others)',
+      column: 0,
     },
     {
       id: 6,
       text: '???',
+      column: 1,
     },
     {
       id: 7,
       text: 'PROFIT',
+      column: 1,
     },
   ])
 
@@ -57,6 +69,23 @@ export const Container: FC = () => {
       })
     )
   }, [])
+
+  type ColumnType = {
+    name: string
+    items: Item[]
+  }
+
+  const [columnInCards, setColumnInCards] = useState<ColumnType[]>([])
+
+  useEffect(() => {
+    const columns = ['Todo', 'Doing', 'Done']
+    setColumnInCards(() => {
+      return columns.map((name, i) => ({
+        name,
+        items: cards.filter((v) => v.column === i),
+      }))
+    })
+  }, [cards])
 
   const renderCard = useCallback(
     (card: { id: number; text: string }, index: number) => {
@@ -73,9 +102,17 @@ export const Container: FC = () => {
     []
   )
 
+  let index = 0
   return (
-    <>
-      <div style={style}>{cards.map((card, i) => renderCard(card, i))}</div>
-    </>
+    <div style={{ display: 'flex' }}>
+      {columnInCards.map((column, i) => (
+        <section key={i} style={style}>
+          <div>{column.name}</div>
+          <div style={{ marginTop: '.5rem', minHeight: '10rem' }}>
+            {column.items.map((card) => renderCard(card, index++))}
+          </div>
+        </section>
+      ))}
+    </div>
   )
 }
